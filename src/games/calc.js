d03aecd89739cf7fd50676fe8e4a62229a7cd7d1
Julie <juliayakutik@gmail.com>
2020-03-16 17:07:21 +0300

    step 6

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,22 +1,53 @@
+import exprEval from 'expr-eval';
 import getRandomInteger from '../utils/getRandomInteger.js';
 import start from '../start.js';
 
-const description = 'Answer "yes" if number is even otherwise answer "no".';
+const description = 'What is the result of the expression?';
 
 /**
- * Checks if number is even
- * @param {Number} num
- * @returns {Boolean}
+ * Generates maths sign
+ * @param {Number} number
+ * @returns {String}
  */
-const isEven = (num) => num % 2 === 0;
+const getRandomSign = (number) => {
+    switch (number) {
+    case 1:
+        return '+';
+
+    case 2:
+        return '-';
+
+    case 3:
+        return '*';
+
+    default:
+        return '+';
+    }
+};
+
+/**
+ * Generates maths expression
+ * @returns {String}
+ */
+const getExpression = () => {
+    const num1 = getRandomInteger(1, 100);
+    const num2 = getRandomInteger(1, 100);
+    const signId = getRandomInteger(1, 3);
+    const sign = getRandomSign(signId);
+
+    return `${num1} ${sign} ${num2}`;
+};
 
 /**
  * Retrieves data needed for game
  * @returns {Object}
  */
 const getData = () => {
-    const question = getRandomInteger(1, 100);
-    const answer = isEven(question) ? 'yes' : 'no';
+    const question = getExpression();
+    const { Parser } = exprEval;
+    const parser = new Parser();
+    const expression = parser.parse(question);
+    const answer = expression.evaluate().toString();
 
     return { question, answer };
 };
